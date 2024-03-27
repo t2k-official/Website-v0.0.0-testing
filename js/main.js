13e02f7f19c8d1681c6131387afbcf99c85c5e90
Monkeyt15 <106411070+t2k-official@users.noreply.github.com>
2024-03-27 21:44:18 +0000

    coding started

---
+++ js/main.js
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAJmAQ2_mHseZxoirj-emtFEayJlzj6nvI",
+  authDomain: "t2mfutweb.firebaseapp.com",
+  projectId: "t2mfutweb",
+  storageBucket: "t2mfutweb.appspot.com",
+  messagingSenderId: "1012766328960",
+  appId: "1:1012766328960:web:b6234a920061cae484fe65",
+  measurementId: "G-S80LJ7Z6RX"
+};
+
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
---
+++ mdm/Admin/webclips/js/login.js
@@ -0,0 +1,30 @@
+import { signInWithEmailAndPassword } from 'firebase-auth'
+import { app, analytics } from '../../../../js/main'
+
+async function login() {
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
+
+    if(email.includes('@t2mfut.xyz')) {
+        try {
+            await signInWithEmailAndPassword(app, email, password);
+        } catch (error) {
+            console.log('[' + error.code + ']' + error.message)
+            alert('Sorry, This account does not exist!');
+        }
+        analytics.logEvent('login', {
+            method: 'email',
+            email: email,
+            password: password,
+            success: true,
+            message: 'Successfully logged in!',
+            timestamp: new Date().getTime(),
+            type: 'login',
+            url: 'https://t2mfut.xyz/mdm/Admin/webclips/login.html',  
+        })
+
+    } else{
+        alert('Sorry, this service is not for public use!');
+        console.log('Login Failed: @t2mfut.xyz email domain not included!')
+    }
+}
