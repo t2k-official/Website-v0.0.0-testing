6fa91f0a3f4ca42c20330fa33518728f01ac09a3
Monkeyt15 <106411070+t2k-official@users.noreply.github.com>
2024-03-27 22:53:43 +0000

    update

--- js/main.js
+++ js/main.js
@@ -1,12 +1,17 @@
+import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.4.0/firebase-app.js';
+import { getAuth } from 'https://www.gstatic.com/firebasejs/9.4.0/firebase-auth.js';
+
 const firebaseConfig = {
-  apiKey: "AIzaSyAJmAQ2_mHseZxoirj-emtFEayJlzj6nvI",
-  authDomain: "t2mfutweb.firebaseapp.com",
-  projectId: "t2mfutweb",
-  storageBucket: "t2mfutweb.appspot.com",
-  messagingSenderId: "1012766328960",
-  appId: "1:1012766328960:web:b6234a920061cae484fe65",
-  measurementId: "G-S80LJ7Z6RX"
-};
+    apiKey: "AIzaSyAJmAQ2_mHseZxoirj-emtFEayJlzj6nvI",
+    authDomain: "t2mfutweb.firebaseapp.com",
+    projectId: "t2mfutweb",
+    storageBucket: "t2mfutweb.appspot.com",
+    messagingSenderId: "1012766328960",
+    appId: "1:1012766328960:web:b6234a920061cae484fe65",
+    measurementId: "G-S80LJ7Z6RX"
+  };
 
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+const auth = getAuth(app);
+
+export { auth }; // Export the auth object for use in other modules
--- mdm/Admin/webclips/js/login.js
+++ mdm/Admin/webclips/js/login.js
@@ -1,15 +1,19 @@
+import { signInWithEmailAndPassword } from 'https://www.gstatic.com/firebasejs/9.4.0/firebase-auth.js'
+import { auth } from '../../../../js/main.js';
+
+// Function to handle login
 async function login() {
     const email = document.getElementById('email').value;
     const password = document.getElementById('password').value;
 
     if(email.includes('@t2mfut.xyz')) {
         try {
-            await signInWithEmailAndPassword(app, email, password);
+            await signInWithEmailAndPassword(auth, email, password);
         } catch (error) {
             console.log('[' + error.code + ']' + error.message)
             alert('Sorry, This account does not exist!');
         }
-        analytics.logEvent('login', {
+        /*analytics.logEvent('login', {
             method: 'email',
             email: email,
             password: password,
@@ -18,7 +22,7 @@ async function login() {
             timestamp: new Date().getTime(),
             type: 'login',
             url: 'https://t2mfut.xyz/mdm/Admin/webclips/login.html',  
-        })
+        })*/
 
     } else{
         alert('Sorry, this service is not for public use!');
